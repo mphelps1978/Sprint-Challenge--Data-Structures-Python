92d82a5cdb1262b0b29b3eee69d34404daded947
Michael Phelps <michaeljphelps1978@gmail.com>
2020-09-27 20:21:45 -0500

    :construction: RingBuffer
    RingBuffer Completed

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,27 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.store = []
+        self.oldest = 0 # references storage list, to make sure we have the oldest item
 
     def append(self, item):
-        pass
+        # first we need to make sure that there is capacity left
+        if len(self.store) < self.capacity:
+            #then we can append it to the ring Buffer
+            self.store.append(item)
+
+        #if we don't have room, we need to make room
+        else:
+            self.store.insert(self.oldest, item) # inserting item after the oldest item
+            self.store.pop(self.oldest + 1) #remove the previous oldest item
+
+            # then we need to update the oldest (if we can)
+            if self.oldest + 1 < self.capacity:
+                self.oldest += 1
+
+            else:
+                self.oldest = 0
 
     def get(self):
-        pass
+        # we can just return the storage list here
+        return self.store
